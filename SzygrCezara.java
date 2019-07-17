3dcfd16b165a4e8c8964823630682bcd3c74b0d4
dpienczyn <donia19881@wp.pl>
2019-07-17 14:53:16 +0200

    poprawiony kod

---
+++ SzygrCezara.java
@@ -0,0 +1,127 @@
+package Szyfr;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class SzygrCezara {
+
+	public void Start() {
+		System.out.println("Program obs³uguje dwa pliki. Z jednego pobiera dane które szyfruje liczba któr¹ wprowadzisz na ekran w konsoli. Nastepnie zaszyfrowane dane zapisuje do pliku drugiego");
+		System.out.println("¯ycze udanej zabawy!");
+	}
+	
+	public String Words() throws FileNotFoundException{
+		String Filepath = "C:/Users/Dominika/eclipse-workspace/Tasks/bin/Szyfr/tekst.txt";
+		File f = new File(Filepath);
+		Scanner load = new Scanner(f);
+		String word;
+		word = load.nextLine();
+		load.close();
+		return word;
+	}
+	
+	public byte[] replace(String word) {
+		byte[] b = word.getBytes();
+		return b;
+	}
+	
+	public int key() {
+		Scanner read = new Scanner(System.in);
+		System.out.println("Podaj klucz w postaci cyfry od 1 do 26..");
+		int key = read.nextInt();
+		return key;
+	}
+	
+	public void compare(int key) {
+		if(key>26||key<1) {
+			System.out.println("Wybierz klucz z przedzia³u od 1 do 26");
+			System.exit(0);
+		}
+	}
+	
+	public void writeEncode(byte[] b, int key) throws FileNotFoundException{
+		String FilePathEncode = "C:/Users/Dominika/eclipse-workspace/Tasks/bin/Szyfr/zakodowany.txt";
+		PrintWriter write = new PrintWriter(FilePathEncode);
+		for(byte n:b) {
+			int h = 0;
+			int keyOne = 65 - key;
+			int keyTwo = 97 - key;
+			if(n==32){
+				h = n;
+			}
+			if(n>=65&&n<=90) {
+				h=65+(n-keyOne)%26;
+			}
+			if(n>=97&&n<=122) {
+				h=97+(n-keyTwo)%26;
+			}
+			write.print((char)h);
+		}
+		write.close();
+	}
+	
+	public char Question() {
+		Scanner read = new Scanner(System.in);
+		System.out.println("Czy chcesz odkodowaæ zadanie? Jeœli tak to wciœnij 't' jeœli nie to wciœnij 'n'");
+		char t = read.next().charAt(0);
+		return t;
+	}
+	
+	public void wordDecode(char t, int key) throws FileNotFoundException{
+		
+		String FilePathDecode = "C:/Users/Dominika/eclipse-workspace/Tasks/bin/Szyfr/zakodowany.txt";
+		String FilePathWrite = "C:/Users/Dominika/eclipse-workspace/Tasks/bin/Szyfr/odkodowany.txt";
+		if(t=='t'){
+			File file = new File(FilePathDecode);
+			Scanner read = new Scanner(file);
+			Scanner p = new Scanner(System.in);
+			String word = read.nextLine();
+			byte[] j = word.getBytes();
+			PrintWriter write = new PrintWriter(FilePathWrite);	
+			for(byte character: j){
+				int a = 0;
+				int k;
+				k= 26-key;
+				int keyThree = 65-k;
+				int keyFour = 97-k;
+				if(character==32)
+				{
+					a=character;
+					System.out.println(character);
+				}
+				if(character>=65&&character<=90)
+				{
+					a=65+(character-keyThree) % 26;
+				}
+				if(character>=97&&character<=122)
+				{
+					a=97+(character-keyFour)%26;
+				}
+				System.out.println((char)a);
+				write.print((char)a);
+			}
+			p.close();
+			write.close();
+			}
+			else 
+			{
+				System.out.println("Koniec kodowania");
+			}
+	}	
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		SzygrCezara szyfr = new SzygrCezara();
+		szyfr.Start();
+		String w = szyfr.Words();
+		byte[] v = szyfr.replace(w);
+		int key = szyfr.key();
+		szyfr.compare(key);
+		szyfr.writeEncode(v, key);
+		char c = szyfr.Question();
+		szyfr.wordDecode(c, key);
+	}
+}
+
